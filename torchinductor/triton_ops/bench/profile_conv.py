6d5a52ac2de25a42b81a5a303967f481aa8b3d87
Yunjie Pan <panyj@fb.com>
2022-06-13 18:05:01 +0000

    update profiles

--- torchinductor/triton_ops/bench/profile_conv.py
+++ torchinductor/triton_ops/bench/profile_conv.py
@@ -1,4 +1,6 @@
+import model
 import torch
+import torchdynamo
 import torchinductor.triton_ops
 
 from torch.profiler import profile, record_function, ProfilerActivity
@@ -8,9 +10,10 @@ torch.backends.cuda.matmul.allow_tf32 = True
 # The flag below controls whether to allow TF32 on cuDNN. This flag defaults to True.
 torch.backends.cudnn.allow_tf32 = True
 
-
-BATCH, IN_C, IN_H, IN_W, KERNEL_N, KERNEL_H, KERNEL_W, stride, padding, dilation, groups, dtype = \
-    32, 56, 56, 64, 3, 3, 64, (1, 1), (0, 0), (1, 1), 1, torch.float32
+BATCH = 32
+IN_H, IN_W, IN_C, KERNEL_H, KERNEL_W, KERNEL_N, stride, padding = 56, 56, 64, 1, 1, 64, (1, 1), (0, 0)
+dilation, groups = (1,1), 1
+dtype=torch.float32
 
 def profile_op(
         # provider
@@ -36,14 +39,35 @@ def profile_op(
     OUT_H = (IN_H + 2 * padding[0] - dilation[0] * (KERNEL_H - 1) - 1 + stride[0]) // stride[0]
     OUT_W = (IN_W + 2 * padding[1] - dilation[1] * (KERNEL_W - 1) - 1 + stride[1]) // stride[1]
 
-    if provider == "cublas":
+    if provider == "aten":
         fn = lambda: torch.conv2d(x, w, bias, stride, padding, dilation, groups)
     if provider == "triton":
         fn = lambda: torchinductor.triton_ops.conv(
             x, w, bias, stride, padding, dilation, False, (0, 0), groups
         )
+    elif provider == "inductor":
+        @torchdynamo.optimize("inductor")
+        def wrap_conv(*args, **kwargs):
+            return torch.conv2d(*args, **kwargs)
+
+        fn = lambda: wrap_conv(x, w, bias, stride, padding, dilation, groups)
     else:
         ValueError(f"{provider} not supported")
+    if provider == "aten":
+        # warmp up for cudagraph
+        s = torch.cuda.Stream()
+        s.wait_stream(torch.cuda.current_stream())
+        with torch.cuda.stream(s):
+            for i in range(3):
+                tmp = fn()
+        torch.cuda.current_stream().wait_stream(s)
+
+        # capture
+        g = torch.cuda.CUDAGraph()
+        with torch.cuda.graph(g):
+            tmp = fn()
+
+        fn = lambda: g.replay()
     # warm up
     for _ in range(warmup):
         fn()
@@ -56,7 +80,7 @@ def profile_op(
     print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
 
 
-for provider in ["cublas", "triton"]:
+for provider in ["aten", "inductor"]:
     profile_op(
         # provider
         provider,
